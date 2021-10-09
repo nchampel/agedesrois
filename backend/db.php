93e6d84705ad8f7dd04c2684d817a845ddecdb54
nchampel <57298158+nchampel@users.noreply.github.com>
2021-10-09 04:20:05 +0200

    Delete db.php
    security

--- backend/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = "mysql:host=localhost;dbname=agedesrois";
-$userDB = "root";
-$pass = '';
-// $url = "mysql:host=mysql-agedesrois.alwaysdata.net;dbname=agedesrois_mabase";
-// $userDB = "245140";
-// $pass = 'Faischier1';
